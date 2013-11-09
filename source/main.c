0b0507dd0c76b5c94e3436a0ba14b571e902d146
Simon Gustavsson <simongustavsson@driveworks.co.uk>
2013-11-09 18:52:41 +0000

    Wakeup byte, size confirmation and memcpy fix

--- source/main.c
+++ source/main.c
@@ -12,6 +12,7 @@ volatile unsigned int gKernelBytesReceived;
 char gBuffer[KERNEL_MAX_SIZE];
 volatile unsigned int gUserConnected;
 unsigned int gBufferIndex;
+volatile unsigned int gWakeupReceived;
 
 static inline void *memcpy(void *dest, const void *src, unsigned int bytesToCopy)
 {
@@ -27,8 +28,17 @@ static inline void *memcpy(void *dest, const void *src, unsigned int bytesToCopy
 
 void c_irq_handler(void)
 {
+	// First accept a random byte to establish connection before receiving kernel size
+	if (!gWakeupReceived)
+	{
+		gWakeupReceived = 1;
+		uart_getc();
+		return;
+	}
+
 	gBuffer[gBufferIndex] = uart_getc();
 
+	// Are we still receiving the size of the kernel?
 	if (gSizeBytesReceived < 4)
 	{
 		gSizeBytesReceived++;
@@ -43,7 +53,10 @@ void c_irq_handler(void)
 			}
 			else
 			{
-				uart_puts("Gimme dat kernel, bro!\n\r");
+				uart_putc(gBuffer[0]);
+				uart_putc(gBuffer[1]);
+				uart_putc(gBuffer[2]);
+				uart_putc(gBuffer[3]);
 			}
 		}
 
@@ -51,17 +64,25 @@ void c_irq_handler(void)
 
 		return;
 	}
-	else
-	{
-		gKernelBytesReceived++;
-	}
+
+	// We're receiving the kernel
+	gKernelBytesReceived++;
 
 	// We done?
 	if (gKernelBytesReceived == gKernelSize)
 	{
+		// TODO: This doesn't work with large kernels currently as we overwrite the current instruction!
+		// TODO: Relocate the bootloader kernel to an address less than 8000 prior to receiving the kernel over serial
 		char* dest = (char*)(0x8000);
-		memcpy(dest, &gBuffer[3], gKernelSize);
+		memcpy(dest, &gBuffer[4], gKernelSize);
 
+		// DEBUG: Print the first 20 bytes of the kernel back to the deployer after the move
+		// So that it can verify that everything went fine
+		unsigned int i;
+		for (i = 0; i < 20; i++)
+			uart_putc(dest[i]);
+
+		// Jump into the new shiny kernel
 		void(*start)(void) = (void(*)())(0x8000);
 		start();
 	}
@@ -74,6 +95,7 @@ void cmain(void)
 	unsigned int i;
 
 	// Zero out the global vars, because I don't trust BSS. <Paranoia />
+	gWakeupReceived = 0;
 	gKernelBytesReceived = 0;
 	gBufferIndex = 0;
 	gSizeBytesReceived = 0;
@@ -92,7 +114,6 @@ void cmain(void)
 
 	// Wait for user to connect
 	while (!gUserConnected) { /* Do Nothing */ }
-
 
 	while (1);
 }
