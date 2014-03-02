b0836ccc5061dc745c350b982063f389c2ed5a6a
Simon Gustavsson <simongustavsson@driveworks.co.uk>
2014-03-02 10:22:31 +0000

    Added additional error handling to deployer

--- deployer/DeployerOfWorlds.cs
+++ deployer/DeployerOfWorlds.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.IO;
 using System.IO.Ports;
 using System.Management;
@@ -11,9 +13,11 @@ namespace PiOSDeployer
         private SerialPort mSerial;
         private byte[] mKernelBuffer;
         private int mKernelSize = 0;
-        private byte[] mReceiveBuffer = new byte[1024];
+        private byte[] mReceiveBuffer = new byte[2048];
         private int mReceiveIndex = 0;
-        
+        private string mReceivedString = String.Empty;
+        private int mPartialReceiveIndex = 0;
+
         public void Run(string pathToKernelImage)
         {
             if (!File.Exists(pathToKernelImage))
@@ -24,34 +28,35 @@ namespace PiOSDeployer
 
             NativeMethods.SetConsoleCtrlHandler(new HandlerRoutine(ConsoleCtrlCheck), true);
 
-            InitializeSerialPort();
-            
+            if (!InitializeSerialPort())
+                return;
+
             try
             {
                 // Send kernel size
                 mKernelSize = (int)new FileInfo(pathToKernelImage).Length;
 
-                // Send kernel
+                // Read kernel into buffer so we can send it
                 mKernelBuffer = new byte[mKernelSize];
                 using (var fs = File.OpenRead(pathToKernelImage))
                 {
                     fs.Read(mKernelBuffer, 0, (int)mKernelSize);
                 }
             }
-            catch(Exception e)
+            catch (Exception e)
             {
                 Console.WriteLine("Could not read kernel from disk, {0}", e.Message);
             }
 
             // Write Size, then the data
-            mSerial.Write(BitConverter.GetBytes(mKernelSize), 0, 4);
+            this.Send(BitConverter.GetBytes(mKernelSize));
 
             Console.Write("Kernel size sent, waiting for confirmation...");
 
             InifiniteCommandLoop();
         }
 
-        private void InitializeSerialPort()
+        private bool InitializeSerialPort()
         {
             int port = -1;
             while (true)
@@ -65,51 +70,85 @@ namespace PiOSDeployer
                 Console.ReadLine();
             }
 
-            mSerial = new SerialPort("COM" + port.ToString(), 115200, Parity.None, 8, StopBits.One);
-            mSerial.DataReceived += (s, e) =>
+            try
             {
-                var bytesToRead = mSerial.BytesToRead; // Decrements when reading, so freeze it
-               
-                for (var i = 0; i < bytesToRead; i++)
-                    mReceiveBuffer[mReceiveIndex++] = (byte)mSerial.ReadByte();
+                mSerial = new SerialPort("COM" + port.ToString(), 115200, Parity.None, 8, StopBits.One);
+            }
+            catch(IOException ex)
+            {
+                Console.WriteLine("Failed to create serial port: {0}", ex.Message);
+            }
+            
+            mSerial.DataReceived += Serial_DataReceived;
 
-                // Was the last one a 0 indicating EOF
-                // NOTE: This means the kernel is currently not allowed to send data!
-                if (mReceiveBuffer[mReceiveIndex - 1] == 0)
+            try
+            {
+                mSerial.Open();
+            }
+            catch(UnauthorizedAccessException)
+            {
+                Console.WriteLine("The COM port is already in use, is another instance of the deployer running?");
+                return false;
+            }
+
+            return true;
+        }
+
+        private void Serial_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            var bytesToRead = mSerial.BytesToRead; // Decrements when reading, so freeze it
+            Debug.WriteLine("Bytes to read: {0}", bytesToRead);
+
+            // Note that we can receive several events with partial data,
+            // Keep receiving data until we get a null terminator
+            for (var i = 0; i < bytesToRead; i++)
+            {
+                // We might receive more data than the buffer can hold
+                if(mReceiveIndex >= mReceiveBuffer.Length - 1)
                 {
-                    var receivedString = Encoding.UTF8.GetString(mReceiveBuffer, 0, mReceiveIndex - 1); // (Don't read \0)
-                    switch (receivedString.ToUpper())
-                    {
-                        case "SIZEOK":
-                            Console.Write(" Done!{0}Sending kernel...", Environment.NewLine);
-
-                            mSerial.Write(mKernelBuffer, 0, mKernelSize);
-
-                            Console.Write(" Done!" + Environment.NewLine);
-                            break;
-                        case "BADSIZE":
-                            Console.WriteLine("Kernel is too big. :(");
-                            break;
-                        case "KRNOK":
-                            Console.WriteLine("Kernel accepted, booting...");
-                            break;
-                        case "STRT":
-                            Console.WriteLine("Bootloader restarted: Sending kernelsize");
-
-                            // Resend the kernel size (kernel is sent in receive handler)
-                            mSerial.Write(BitConverter.GetBytes(mKernelSize), 0, 4);
-                            break;
-                        default:
-                            Console.Write(receivedString);
-                            break;
-                    }
-
+                    mReceivedString += Encoding.UTF8.GetString(mReceiveBuffer, 0, mReceiveIndex - 1);
+                    Array.Clear(mReceiveBuffer, 0, mReceiveBuffer.Length);
                     mReceiveIndex = 0;
-                    Array.Clear(mReceiveBuffer, 0, 40);
                 }
-            };
 
-            mSerial.Open();
+                mReceiveBuffer[mReceiveIndex++] = (byte)mSerial.ReadByte();
+            }
+            // Was the last one a 0 indicating EOF
+            // NOTE: This means the kernel is currently not allowed to send data!
+            if (mReceiveBuffer[mReceiveIndex - 1] != 0)
+                return; // We got more to receive yet
+
+            mReceivedString += Encoding.UTF8.GetString(mReceiveBuffer, 0, mReceiveIndex - 1); // (Don't read \0)
+            mReceivedString = mReceivedString.Replace("\0", "");
+
+            switch (mReceivedString.ToUpper())
+            {
+                case "SIZEOK":
+                    Console.Write(" Done!{0}Sending kernel...", Environment.NewLine);
+
+                    this.Send(mKernelBuffer); // Do we need to pass in kernel size?
+
+                    Console.Write(" Done!" + Environment.NewLine);
+                    break;
+                case "BADSIZE": Console.WriteLine("Kernel is too big. :("); break;
+                case "KRNOK": Console.WriteLine("Kernel accepted, booting..."); break;
+                case "STRT":
+                    // Occurs when the Pi is powercycled - Redeploy kernel
+                    Console.WriteLine("Bootloader restarted: Sending kernelsize");
+
+                    // Resend the kernel size (kernel is sent in receive handler)
+                    this.Send(BitConverter.GetBytes(mKernelSize));
+                    break;
+                default:
+                    Console.Write(mReceivedString);
+                    break;
+            }
+
+            mReceivedString = String.Empty;
+
+            // Clear receive buffer in preparation of more data
+            mReceiveIndex = 0;
+            Array.Clear(mReceiveBuffer, 0, 40);
         }
 
         private void InifiniteCommandLoop()
@@ -118,10 +157,10 @@ namespace PiOSDeployer
             {
                 var key = Console.ReadKey(true);
 
-                if(key.Key == ConsoleKey.Escape)
+                if (key.Key == ConsoleKey.Escape)
                     break;
 
-                mSerial.Write(new byte[] { (byte)key.KeyChar }, 0, 1);
+                this.Send(new byte[] { (byte)key.KeyChar });
             }
         }
 
@@ -131,7 +170,7 @@ namespace PiOSDeployer
             {
                 var caption = obj["Caption"] as string;
 
-                if (caption!= null && caption.IndexOf("Prolific") > -1)
+                if (caption != null && caption.IndexOf("Prolific") > -1)
                 {
                     var comPort = caption.Substring(caption.IndexOf("COM") + 3, 1);
                     int com;
@@ -149,12 +188,28 @@ namespace PiOSDeployer
         {
             if (ctrlType == CtrlTypes.CTRL_CLOSE_EVENT)
             {
-                // Tell PiOS to restart so that we can reconnect to the bootloader
-                mSerial.Write(new byte[] { (byte)'x', }, 0, 1);
+                // Tell PiOS to restart so that it's ready to receive a new kernel
+                this.Send(new byte[] { (byte)'x' });
             }
 
-            // Put your own handler here
             return true;
+        }
+
+        private void Send(byte[] data)
+        {
+            if (!mSerial.IsOpen)
+            {
+                Console.WriteLine("Could not write data as serial port is closed.");
+                return;
+            }
+            try
+            {
+                mSerial.Write(data, 0, data.Length);
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("Failed to write to serial, {0}.", ex.Message);
+            }
         }
     }
 }
